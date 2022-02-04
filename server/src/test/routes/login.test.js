1516c9bbfa60481033cc76f348cd4405d8132e43
Abdelghafour Barane <abdelghafourbarane@gmail.com>
2022-02-04 21:27:20 +0100

    add login test without using redis-mock

---
+++ server/src/test/routes/login.test.js
@@ -0,0 +1,46 @@
+const request = require("supertest");
+const { redisClient } = require("../../services/redis");
+
+const { app } = require("../../app");
+
+describe("test login service", () => {
+  const mockUser = {
+    username: "af",
+    password: "12345",
+  };
+
+  let server = null;
+  beforeAll((done) => {
+    server = app.listen(4000);
+    done();
+  });
+
+  afterAll(async () => {
+    await redisClient.quit();
+    await server.close();
+  });
+
+  it("should respond with token object", (done) => {
+    request(server)
+      .post("/login")
+      .send(mockUser)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({ token: expect.any(String) })
+        );
+        return done();
+      });
+  });
+
+  it("home should respond with 404 status code", (done) => {
+    request(server).get("/").expect(404, done);
+  });
+  it("nothing should respond with 404 status code", (done) => {
+    request(server).get("/nothing").expect(404, done);
+  });
+  it("walo should respond with 404 status code", (done) => {
+    request(server).get("/walo").expect(404, done);
+  });
+});
